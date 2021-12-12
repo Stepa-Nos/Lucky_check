9bbb27e174d85359e4124958cc827e35394ac4c7
Stepa-Nos <nosotepa@yandex.ru>
2021-12-13 02:31:33 +0300

    main.go

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main () {
+	// Приветствие
+	fmt.Println("Привет, хочешь проверить свою удачу?")
+	// Нужно спрашивать пользователя и провеять его ответ через свич-кейс. Типа, Да\Нет
+	fmt.Print("Я загадал число от 1 до 10, но у тебя будет только 3 попытки\n", "\n")
+	fmt.Println("Думаешь сможешь угадать? ")
+	fmt.Println("Введи своё число и проверь это :)")
+
+	// Генерация числа за счёт времени
+	seed_number:= time.Now().Unix()
+	rand.Seed(seed_number)
+	my_random_number:= rand.Intn(10)+1
+
+	lucky := false // очень нужна для правильного срабатывания цикла при удачи
+
+	// Цикла и кол-во попыток
+	for compare := 0;compare<3; compare++{
+		fmt.Println("Осталось попыток: ", 3-compare)
+
+		// получение номера пользователя
+		var user_number int
+		fmt.Scan(&user_number)
+
+		if my_random_number > user_number{
+			fmt.Println("Ты не угадал, твоё число меньше")
+		} else if  my_random_number < user_number{
+			fmt.Println("Ты не угадал, твоё число больше")
+		} else {
+			lucky = true
+			fmt.Println("Вау, твоя удача - это нечто! Поздравляю ;)")
+			break
+		}
+	}
+	if !lucky{ // Переводит из false в true, для срабатывания
+		fmt.Println("Вау, твоя удача - это нечто! Ты проиграл :( ")
+		fmt.Println("Моё число было", my_random_number, "\n")
+	}
+}
